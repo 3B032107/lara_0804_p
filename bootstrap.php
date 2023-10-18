a17ed550cfcf273cf9383e27f283c6a21635b8b9
廖家駒 <3b032107@gm.student.ncut.edu.tw>
2023-10-18 16:40:39 +0800

    設定autoload

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 33060);
-define('DB_DATABASE', '');
+define('DB_DATABASE', 'lara_0804_p');
 define('DB_CHARSET', 'utf8');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
